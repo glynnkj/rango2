fc9c1935cdf31ef63afd09db3fced863760f85e9
glynnkj <glynnkatiej@gmail.com>
2017-03-15 15:56:30 +0000

    Create models.py

---
+++ rango/models.py
@@ -0,0 +1,46 @@
+from __future__ import unicode_literals
+
+from django.db import models
+from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
+# Create your models here.
+# Inherit from the Model base class, django.db.models.Model.
+
+
+class Category(models.Model):
+	name = models.CharField(max_length=128, unique=True)
+	views = models.IntegerField(default=0)
+	likes = models.IntegerField(default=0)
+	slug = models.SlugField(blank=True)
+
+	def save(self, *args, **kwargs):
+		self.slug = slugify(self.name)
+		super(Category, self).save(*args, **kwargs)
+=
+
+	class Meta:
+		verbose_name_plural = 'Categories'
+
+	def __str__(self): # For Python 2, use __unicode__ too
+		return self.name
+
+class Page(models.Model):
+	category = models.ForeignKey(Category)
+	title = models.CharField(max_length=128)
+	url = models.URLField()
+	views = models.IntegerField(default=0)
+
+	def __str__(self): # For Python 2, use __unicode__ too
+		return self.title
+
+class UserProfile(models.Model):
+    # This line links UserProfile to a User model instance.
+    user = models.OneToOneField(User)
+
+    # The additional attributes we wish to include.
+    website = models.URLField(blank=True)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+
+    def __str__(self):
+    
+return self.user.username
